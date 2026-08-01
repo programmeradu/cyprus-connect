85eeb62456a2efc4cb796a9852984764b08ce9f8
Samuel Adu-Berekorang <samueladu1970@gmail.com>
2026-08-01 16:02:49 +0000

    fix: remove distDir from next.config.ts so OpenNext finds standard .next build directory

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
-  distDir: "dist",
   outputFileTracingRoot: path.join(process.cwd()),
   images: {
     remotePatterns: [
